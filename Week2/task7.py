bcecdeb26151112b5ffdbbc9c547c8033a407483
mohit8890 <raom99885@gmail.com>
2025-06-15 22:46:13 +0530

    Introduction to Sets

--- Week2/task7.py
+++
@@ -1,9 +0,0 @@
-T = int(input())
-for _ in range(T):
-    try:
-        a, b = input().split()
-        print(int(a) // int(b))
-    except ZeroDivisionError as e:
-        print("Error Code:", e)
-    except ValueError as e:
-        print("Error Code:", e)
